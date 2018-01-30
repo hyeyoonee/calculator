d7de090d78c17eb0a8b900a3dc3b94f990324807
Jiwon-PC\Jiwon <sinzip@gmail.com>
2018-01-30 15:41:40 +0900

    #7 modify

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 	int a, b, c;
 	char operator;
 	
-	printf("Please enter a expression");
+	printf("Please enter a ");
 	scanf("%d %c %d", &a, &operator, &b);
 	
 	switch (operator)
